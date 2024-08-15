95ef5bbf6b6342ef42a7f49c0a5736515c0ba086
Amit Kumar <152176527+4miit@users.noreply.github.com>
2024-08-15 23:38:24 +0530

    new commit

--- src/post.tsx
+++ src/post.tsx
@@ -1,42 +1,31 @@
-
 import Slider from "./imgSlider";
 import axios from "axios";
-import { useEffect, useState } from "react";  
+import { useEffect, useState } from "react";
 
-interface imageData{
-     download_url : string;
+interface imageData {
+  download_url: string;
 }
 
-
 export default function Post() {
+  const [data, setData] = useState<imageData[]>([]);
 
-const[data , setData] = useState <imageData[]>([]);
+  const [page, setPage] = useState<number>(1);
 
-const [page, setPage] = useState<number>(1);
-
-useEffect( () => {
- axios.get<imageData[]>(`https://picsum.photos/v2/list?page=${page}&limit=5`).then( (arr)  =>
-   setData(arr.data)
- )
-} , [page])
+  useEffect(() => {
+    axios
+      .get<imageData[]>(`https://picsum.photos/v2/list?page=${page}&limit=5`)
+      .then((arr) => setData(arr.data));
+  }, [page]);
 
   return (
     <div className="container">
-      
-        
-        <div className="post-container">
-        
-        {
-          data.map( (item,_)  => {
-              return <img src={item.download_url} />;
-          })
-        }
-        
-        </div>
+      <div className="post-container">
+        {data.map((item, _) => {
+          return <img src={item.download_url} />;
+        })}
+      </div>
 
-        
-      <Slider page ={page} setPage={setPage}/>
-      
+      <Slider page={page} setPage={setPage} />
     </div>
   );
 }
